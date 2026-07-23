f74a9ffc70f6710ee31501834aa45ace6bd50ace
luoluoqixi <luoqixi@luoqixi.com>
2026-07-24 03:12:24 +0800

    ci: metro cache disable

--- examples/app/metro.config.js
+++ examples/app/metro.config.js
@@ -1,9 +1,16 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
+const isProd = process.env.NODE_ENV === "production";
+console.log(`metro prod: ${isProd}`);
+
 const defaultConfig = getDefaultConfig(__dirname);
 
 const config = {
   ...defaultConfig,
 };
+
+if (isProd) {
+  config.cacheStores = [];
+}
 
 module.exports = config;
